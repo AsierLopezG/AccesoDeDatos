8da771f80c38b65dc54cc86681ca2686c46a017d
cokeIkigai <coke_vader@hotmail.com>
2025-12-05 11:54:47 +0100

    Create Aula entity with attributes and constructors
    Added Aula class with attributes and constructors.

---
+++ demo/service/demo/controller/demo/controller/demo/entity/Aula.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+
+public class Aula {
+
+   
+    private Long id;
+
+    // añadir mínimo 3 atributos (ejemplo: nombre, capacidad, edificio)
+
+    // constructor vacío
+    // constructor con parámetros
+    // getters y setters
+}
